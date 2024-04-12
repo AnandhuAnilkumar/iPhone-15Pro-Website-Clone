33fa502496522941d79b67cbf0821202a0f1393a
Anandhu Anilkumar <anandhuae03@gmail.com>
2024-04-12 20:57:17 +0530

    IPhone 3D models

--- src/components/ModelView.jsx
+++ src/components/ModelView.jsx
@@ -1,10 +1,11 @@
 import { Html, PerspectiveCamera, View } from "@react-three/drei"
 import Lights from "./Lights";
+import IPhone from "./IPhone";
 import { Suspense } from "react";
 
 
 const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, size, item}) => {
-    console.log("ModelView component props:", item);
+    // console.log("ModelView component props:", item);
     return (
     <View index={index} id={gsapType} className={`border-2 border-red-500 w-full h-full ${index === 2} ? 'right-[-100%] : '' `}>
         
@@ -16,7 +17,7 @@ const ModelView = ({ index, groupRef, gsapType, controlRef, setRotationSize, siz
         <Lights/>
 
         <Suspense fallback={<Html><div>Loading</div></Html>}>
-           
+           <IPhone />
         </Suspense>
 
 
